fc11e9c8ecfb3023275697f82d69004cd2cee6a3
Valinotti <dvalinotti@galaxy.lan>
2019-07-09 17:40:21 -0400

    updated to react and build

---
+++ precache-manifest.25b7b8e2d859cef06445ae978ffa2c9e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c484b3c9692c077d7675325e757d17ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dce91bb353f627889b0",
+    "url": "/static/css/main.8fbf7e91.chunk.css"
+  },
+  {
+    "revision": "e8a7abc723c5463c01b7",
+    "url": "/static/js/2.73cff93b.chunk.js"
+  },
+  {
+    "revision": "3dce91bb353f627889b0",
+    "url": "/static/js/main.f60c00d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "190a742c537d254d8c3b2a2c00bc38a1",
+    "url": "/static/media/angular.190a742c.png"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/static/media/cpp.f278be79.png"
+  },
+  {
+    "revision": "423b97b41c8b420d28e84f9b07a530ec",
+    "url": "/static/media/fe.423b97b4.png"
+  },
+  {
+    "revision": "a7fd72b89f1b1bba4ec9b4200bdeb288",
+    "url": "/static/media/java.a7fd72b8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/static/media/mongo.09ed2051.png"
+  },
+  {
+    "revision": "73d4cb1775c200f3072de27f018fe47c",
+    "url": "/static/media/node.73d4cb17.png"
+  },
+  {
+    "revision": "a1c11146ba58d21cf1bbb4f22dce4b89",
+    "url": "/static/media/python.a1c11146.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "8e766e9562509cb3fa14264580c5c0f9",
+    "url": "/static/media/sass.8e766e95.svg"
+  },
+  {
+    "revision": "8753f691eac33e75e46447d2d10f254c",
+    "url": "/static/media/site-bg.8753f691.jpg"
+  },
+  {
+    "revision": "229ed79927c3992dc1d7a00343cf9952",
+    "url": "/static/media/spring.229ed799.png"
+  },
+  {
+    "revision": "a0a4a3638eac642c1099125bfc8e0618",
+    "url": "/static/media/wic.a0a4a363.png"
+  }
+]);
--- src/content.js
+++
@@ -1,8 +0,0 @@
-export const content = [
-  {
-    title: 'Words in Color',
-    image: require('./images/wic.png'),
-    content: 'A single-page web application built in react that takes text input from the user and transforms it into a color palette using a custom-built color conversion algorithm.',
-    github: 'https://github.com/dvalinotti/Words-in-Color'
-  }
-]
