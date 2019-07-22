58dfe58a9724947f5cacb00874c546271d0a6c9b
Valinotti <dvalinotti@galaxy.lan>
2019-07-22 10:09:07 -0400

    Added projects

---
+++ precache-manifest.0183783d11bdb30439b4e040312633e6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db679077a06ec0ed6c19044383f33876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9aba0d989c611bb16be",
+    "url": "/static/css/main.9a6498ee.chunk.css"
+  },
+  {
+    "revision": "5d80312a5ec21d235c78",
+    "url": "/static/js/2.83a6af18.chunk.js"
+  },
+  {
+    "revision": "c9aba0d989c611bb16be",
+    "url": "/static/js/main.f715346d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "190a742c537d254d8c3b2a2c00bc38a1",
+    "url": "/static/media/angular.190a742c.png"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/static/media/cpp.f278be79.png"
+  },
+  {
+    "revision": "423b97b41c8b420d28e84f9b07a530ec",
+    "url": "/static/media/fe.423b97b4.png"
+  },
+  {
+    "revision": "3c9135b09adac2ce37500ef8ef1d0466",
+    "url": "/static/media/github_icon.3c9135b0.svg"
+  },
+  {
+    "revision": "1f906c3877dd3dab85a8624e4a6fa3fb",
+    "url": "/static/media/isbrooklyn.1f906c38.png"
+  },
+  {
+    "revision": "a7fd72b89f1b1bba4ec9b4200bdeb288",
+    "url": "/static/media/java.a7fd72b8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/static/media/mongo.09ed2051.png"
+  },
+  {
+    "revision": "73d4cb1775c200f3072de27f018fe47c",
+    "url": "/static/media/node.73d4cb17.png"
+  },
+  {
+    "revision": "9732c41a9a86deafa8ef5eceac2cbffc",
+    "url": "/static/media/play-button.9732c41a.svg"
+  },
+  {
+    "revision": "a1c11146ba58d21cf1bbb4f22dce4b89",
+    "url": "/static/media/python.a1c11146.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "58941bc7b468ef0d849321c8dae47c02",
+    "url": "/static/media/reddit.58941bc7.jpg"
+  },
+  {
+    "revision": "3319d5732584babad3baf5819311811f",
+    "url": "/static/media/rv.3319d573.png"
+  },
+  {
+    "revision": "8e766e9562509cb3fa14264580c5c0f9",
+    "url": "/static/media/sass.8e766e95.svg"
+  },
+  {
+    "revision": "8680690eea79b7c795342920bbcbc942",
+    "url": "/static/media/site-bg.8680690e.png"
+  },
+  {
+    "revision": "229ed79927c3992dc1d7a00343cf9952",
+    "url": "/static/media/spring.229ed799.png"
+  },
+  {
+    "revision": "7f735d68a3b3e63272d5cb2ffe4ca891",
+    "url": "/static/media/vivoom.7f735d68.jpg"
+  },
+  {
+    "revision": "a0a4a3638eac642c1099125bfc8e0618",
+    "url": "/static/media/wic.a0a4a363.png"
+  },
+  {
+    "revision": "4d9d0be5b2452fd11c19d3e4458d8c76",
+    "url": "/static/media/womendeliver.4d9d0be5.jpg"
+  }
+]);
